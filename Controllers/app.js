a450797d207a2dbed87b1529cc8053640c883031
erick <erickarmandogarcia@hotmail.com>
2015-04-27 09:30:04 -0500

    Add updateMessage method to the controller.

--- Controllers/app.js
+++ Controllers/app.js
@@ -1,4 +1,8 @@
 angular.module('app',[]);
 angular.module('app').controller('MainCtrl',function($scope){
   $scope.message='hello';
+    $scope.updateMessage = function(message){
+        $scope.message = message;
+    };
+  }
 })
