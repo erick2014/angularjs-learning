f45030820bd9ef39eda54b9501813912fbdbf3b4
Erick Garcia <erickarmandogarcia@hotmail.com>
2015-04-27 14:27:53 -0500

    Add PostCtrl into app.js and index.html files.

--- Services/app.js
+++ Services/app.js
@@ -2,11 +2,23 @@ angular.module('app',[]);
 
 //create a controller called ListCtrl that injects our messages service, and exposes 
 //the list from our service to our view.
-angular.module('app').controller("ListCtrl",function(messages){
-	var self=this;
-	self.messages=messages.list;
-})
+angular.module('app').controller("ListCtrl",['messages', 
+	function(messages){
+		var self=this;
+		self.messages=messages.list;
+	}
+])
 
+angular.module('app').controller("PostCtrl",['messages',
+	function(messages){
+		var self=this;
+		self.newMessage="Hello world";
+		self.addMessage=function(message){
+			messages.add(message);
+			self.newMessage="";
+		}
+	}
+]);
 
 //create our service, called messages
 angular.module('app').factory('messages',function(){
