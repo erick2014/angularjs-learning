6c2ead6cc967abbd0a979835aeb49434448e258e
erick <erickarmandogarcia@hotmail.com>
2015-04-28 11:01:22 -0500

    Add tab directive.

---
+++ Directives/app4.js
@@ -0,0 +1,13 @@
+;(function (window){
+  angular.module('app',[])
+  //define directives here
+  .directive('tab',function(){
+    return {
+      restrict:'E',
+      transclude:true,
+      template:'<h2>Hello world!</h2><div role="tabpanel" ng-transclude></div>',
+      scope:{},
+      link:function(scope,elem,attr){}
+    }
+  })
+})(window)
