fbc82400623adc0a09141d1101f4dae9074dd5e3
erick <erickarmandogarcia@hotmail.com>
2015-04-27 19:44:42 -0500

    Refactoring code,Send values to the directive.

--- Directives/app.js
+++ Directives/app.js
@@ -1,17 +1,17 @@
 angular.module('functionalities',[])
 
 .directive("entering",function(){
-  return function(scope,element){
+  return function(scope,element,attrs){
     element.bind("mouseenter",function(){
-      element.addClass("activeClass");
+      element.addClass(attrs.entering);
     })
   }
 })
 
 .directive("leaving",function(){
-  return function(scope,element){
+  return function(scope,element,attr){
     element.bind("mouseleave",function(){
-      element.removeClass("activeClass");
+      element.removeClass(attrs.entering);
     })
   }
 })
