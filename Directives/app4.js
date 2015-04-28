3b09922f570b7220486996c2350e083839d42e17
erick <erickarmandogarcia@hotmail.com>
2015-04-28 11:18:55 -0500

    Add directive tabset

--- Directives/app4.js
+++ Directives/app4.js
@@ -1,4 +1,5 @@
 ;(function (window){
+
   angular.module('app',[])
   //define directives here
   .directive('tab',function(){
@@ -8,6 +9,21 @@
       template:'<h2>Hello world!</h2><div role="tabpanel" ng-transclude></div>',
       scope:{},
       link:function(scope,elem,attr){}
+    }
+  })
+
+  .directive('tabset',function(){
+    return {
+      restrict:'E',
+      transclude:true,
+      scope:{},
+      templateUrl:"tabset.html",
+      bindToController:true,
+      controllerAs:'tabset',
+      controller:function(){
+        var self=this;
+        self.tabs=[]
+      }
     }
   })
 })(window)
