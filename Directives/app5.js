814e10116a1a140e31c166f3d395836d35c2544b
erick2014 <erickarmandogarcia@hotmail.com>
2015-04-29 10:48:47 -0500

    Directive communication with multiple controllers.

--- Directives/app5.js
+++ Directives/app5.js
@@ -13,16 +13,22 @@ app.directive("country",function(){
 
 app.directive('state',function(){
   return{
-    restrict:"E"
+    restrict:"E",
+    controller:function(){
+      this.makeLaw=function(law){
+        console.log("Law: "+law);
+      }
+    }
   }
 })
 
 app.directive("city", function () {
   return {
     restrict: "E",
-    require: "^country",
-    link: function (scope, element, attrs, countryCtrl) {
-      countryCtrl.makeAnnouncement("This city rocks");
+    require: ["^country","^state"],
+    link: function (scope, element, attrs, ctrls) {
+      ctrls[0].makeAnnouncement("This city rocks");
+      ctrls[1].makeLaw("Jump higher");
     }
   };
 });
