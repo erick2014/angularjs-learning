bbccc7d57f28876f1027a5c986f2df2ecccb07fd
erick <erickarmandogarcia@hotmail.com>
2015-04-29 10:13:58 -0500

    Add container directive.

--- Components_Containers/app.js
+++ Components_Containers/app.js
@@ -9,3 +9,16 @@ app.directive("clock",function(){
     template:"<div>12:00pm {{timezone}}</div>"
   }
 })
+
+app.directive("panel",function(){
+  return {
+    restrict:"E",
+    transclude:true,
+    scope:{
+      title:"@"
+    },
+    template:"<div style='border:3px solid #000000'>"+
+             "<div class='alert-box'>{{title}}</div>"+
+             "<div ng-transclude></div></div>"
+  };
+});
